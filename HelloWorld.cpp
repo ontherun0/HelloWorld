9aae57cc98f2145b1437482539f4b552ff90af97
ontherun <zbhao@bupt.edu.cn>
2023-08-19 15:20:26 +0800

    hello

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
 
+void swap(int& a, int& b) {
+  int temp = a;
+  a = b;
+  b = temp;
+}
+
 int main() {
   cout << "Hello, world!" << endl;
+  int a = 1;
+  int b = 2;
+  int& aa = a;
+  int& bb = b;
+  swap(aa, bb);
+  cout << "a = " << a << ", b = " << b << endl;
   return 0;
 }
